36639a6f9dd62257b4d9e486e5fd14a6d1d201bf
NathanJohnstonn <132169663+NathanJohnstonn@users.noreply.github.com>
2026-03-19 17:40:52 -0700

    set up basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import { Home } from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return (
+        <>
+            <div>Nate Johnston</div>
+        </>
+    )
+}
